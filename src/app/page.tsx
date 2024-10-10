5dfccb19fbbdf6788072ef27f8b3957755713978
Mathis <mtb.mathis@gmail.com>
2024-10-10 10:10:18 +0200

    Homepage Improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,12 @@
 import ListingSearch from "~/components/ListingSearch";
 import ListingCover from "~/components/listing-cover";
 import { api } from "~/trpc/react";
-import ListingPageSkeleton from "~/components/listings/ListingsPageSkeleton";
+import ListingPageSkeleton, {
+  ListingCoverSkeleton,
+} from "~/components/listings/ListingsPageSkeleton";
 import React from "react";
+import CustomButton from "~/components/Buttons/CustomButton";
+import { Button } from "~/components/ui/button";
 
 export default function Rooms() {
   const listings = api.vacationhome.findMany.useInfiniteQuery(
@@ -17,7 +21,7 @@ export default function Rooms() {
   const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage } =
     listings;
 
-  if (isFetching || !listings.data) {
+  if ((isFetching && !listings.data) || !listings.data) {
     return <ListingPageSkeleton></ListingPageSkeleton>;
   }
   return (
@@ -39,7 +43,23 @@ export default function Rooms() {
             );
           });
         })}
-        <button onClick={() => fetchNextPage()}>Load More</button>
+      </div>
+      {isFetching && listings.data ? (
+        <div className="grid grid-cols-4 gap-x-6 gap-y-10">
+          {[...Array(8)].map((_, index) => (
+            <ListingCoverSkeleton key={index} />
+          ))}
+        </div>
+      ) : (
+        <></>
+      )}
+      <div className="mt-6 flex w-full justify-center">
+        <Button
+          className="h-10 w-32 rounded-full bg-transparent font-medium text-black ring-1 ring-gray-300 hover:bg-gray-100"
+          onClick={() => fetchNextPage()}
+        >
+          Load More
+        </Button>
       </div>
     </>
   );
