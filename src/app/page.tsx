c53a5b4d7d7950269b9d7faee0e06770f133b7a3
Mathis <mtb.mathis@gmail.com>
2024-10-10 12:17:07 +0200

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Rooms() {
       <div className="flex w-full justify-center">
         <ListingSearch></ListingSearch>
       </div>
-      <div className="grid grid-cols-4 gap-x-6 gap-y-10">
+      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {data?.pages.map((page) => {
           return page.vacationHomes.map((vacationHome) => {
             return (
