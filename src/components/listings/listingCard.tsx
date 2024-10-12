49586b0310c7d1db9b313cb7d3e24972240e13d1
Mathis <mtb.mathis@gmail.com>
2024-10-12 15:30:15 +0200

    delete Listing

--- src/components/listings/listingCard.tsx
+++ src/components/listings/listingCard.tsx
@@ -4,14 +4,59 @@ import CustomButton from "../Buttons/CustomButton";
 import { CardTitle, CardDescription } from "../ui/card";
 import Image from "next/image";
 import { VacationHomeWithImages } from "~/app/utils/types";
+import useDialogStore from "~/stores/dialogStore";
 import { Trash2 } from "lucide-react";
+import { api } from "~/trpc/react";
+import { notify } from "~/app/utils/notification";
+import { useEffect, useState } from "react";
+import { revalidatePath } from "next/cache";
 
 type Props = {
   vacationHome: VacationHomeWithImages;
   index: number;
   bookings: Booking[];
 };
+
 export default function ListingCard({ vacationHome, bookings, index }: Props) {
+  const id = vacationHome.id;
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  const mutation = api.vacationhome.delete.useMutation({
+    onSuccess: () => {
+      notify.success("Listing erfolgreich gelösscht");
+      setIsDeleted(true);
+    },
+    onError: (error) => {
+      if (error.data?.code === "UNAUTHORIZED") {
+        notify.error(
+          "Sie sind nicht berechtigt, dieses Ferienhaus zu löschen.",
+        );
+      } else {
+        notify.error(
+          "Fehler beim Löschen des Listings. Bitte versuchen Sie es erneut.",
+        );
+      }
+    },
+  });
+
+  const { showDialog } = useDialogStore();
+  const deleteListing = () => {
+    // e.preventDefault();
+    showDialog(
+      "destructive",
+      "Listing Löschen",
+      "confirmation",
+      "Sind sie sicher, dass sie das Listing löschen wolen? Diese Aktion ist nicht umkehrbar. Alle Informationen, die zu diesem Listing gehören, werden dauerhaft und unverzüglich gelöscht.",
+      () => {
+        mutation.mutate({ id });
+      },
+    );
+  };
+
+  if (isDeleted) {
+    return <></>;
+  }
+
   return (
     <div className="grid grid-cols-1">
       <div
@@ -68,7 +113,12 @@ export default function ListingCard({ vacationHome, bookings, index }: Props) {
               Ansehen
             </CustomButton>
           </div>
-          <CustomButton className="h-full" variant="tertiary" fullWidth={false}>
+          <CustomButton
+            className="h-full"
+            variant="tertiary"
+            onClick={deleteListing}
+            fullWidth={false}
+          >
             {/*<Trash2 className="h-4"></Trash2>*/ "Löschen"}
           </CustomButton>
         </div>
